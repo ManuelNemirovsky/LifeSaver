abfe66f3f629ad1ff48ece90a8d08e3e3f50ec07
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2016-08-19 14:46:15 +0300

    adding files

---
+++ app/src/main/java/com/example/user/lifesaver/Chat.java
@@ -0,0 +1,12 @@
+package com.example.user.lifesaver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chat extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
